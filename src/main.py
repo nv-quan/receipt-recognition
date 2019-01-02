1bd1c9e00cb0c2667a68257ea662e2802f2f5dc5
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-01-02 22:12:13 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,9 +7,7 @@ def getDistance(a,b):
 def getOrderPoints(points):
     #divide points into 2 part: left and right
     #sort the points based on x-coordinates
-    print(points)
     sortArgX = np.argsort(points[:,0]) 
-    print('sa: ' + str(sortArgX))
     left = np.array([points[x] for x in sortArgX[0:2]])
     right = np.array([points[x] for x in sortArgX[2:4]])
     #point with bigger y is bottomLeft and vice versa
@@ -48,10 +46,6 @@ if len(res) > 0:
 else:
     print("Contours not found")
 tl, tr, br, bl = getOrderPoints(np.array([x[0] for x in result]))
-print('tl: ' + str(type(tl)))
-print('tr: ' + str(tr))
-print('br: ' + str(br))
-print('bl: ' + str(bl))
 cv.putText(img, "A", tuple(tl), cv.FONT_HERSHEY_PLAIN, 2.0, (0,0,255))
 cv.putText(img, "B", tuple(tr), cv.FONT_HERSHEY_PLAIN, 2.0, (0,0,255))
 cv.putText(img, "C", tuple(br), cv.FONT_HERSHEY_PLAIN, 2.0, (0,0,255))
