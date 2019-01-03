33623d40fbabb0a088511ec8c31ae9e2371d13ed
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-01-03 16:08:31 +0700

    Add debug file

--- src/debug.py
+++ src/debug.py
@@ -5,7 +5,11 @@ import preprocess as pre
 
 inp = sys.argv[1]
 img = cv.imread(inp)
-processed = pre.changeView(img)
+debug = list()
+processed = pre.changeView(img, 0.01, debug)
 cv.imshow('Preprocessed', processed)
 cv.waitKey(0)
 cv.destroyAllWindows()
+cv.imwrite('./output/edges.png', debug[0])
+cv.imwrite('./output/contours.png', debug[1])
+cv.imwrite('./output/detected.png', debug[2])
