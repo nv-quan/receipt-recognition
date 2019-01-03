2035ead68708300b7a0cd13176d18fc5ac146da5
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-01-03 15:47:13 +0700

    Add debug file

---
+++ src/debug.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import numpy as np
+import sys
+import preprocess as pre
+
+inp = sys.argv[1]
+img = cv.imread(inp)
+processed = pre.changeView(img)
+cv.imshow('Preprocessed', processed)
+cv.waitKey(0)
+cv.destroyAllWindows()
