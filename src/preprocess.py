775c6658ec98ebe2064e5839450d33ecba2b7e94
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-01-03 15:32:40 +0700

    Modulize

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,9 @@
+import cv2 as cv
+import numpy as np
 def getDistance(a,b):
     return np.linalg.norm(a - b)
-    
+def getArea(contour):
+    return abs(cv.contourArea(contour))
 def getOrderPoints(points):
     #divide points into 2 part: left and right
     #sort the points based on x-coordinates
@@ -21,44 +24,52 @@ def getOrderPoints(points):
     return (topLeft, topRight, bottomRight, bottomLeft)
 
 def changeView(img, error = 0.02, debug = None):
-    #debug is an empty list that will contain edges and contours
+    #debug is a list to be append as below:
+    # debug = [edges_img, all_contours_img, detected_contour_img]
     #error is the error in Polygon approximation
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray, (5,5), 0)
     edges = cv.Canny(blur, 50, 200)
     im2 , contours, hierarchy  = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    res = list()
-    # minarea = img.shape[0] * img.shape[1] / 2
+    quadri = list()
     for contour in contours:
         epsilon = error * cv.arcLength(contour, True)
         approx = cv.approxPolyDP(contour, epsilon, True)
         if len(approx) == 4:
-            res.append(approx)
-    if len(res) > 0:
-        result = max(res, key = cv.contourArea)
-        cv.drawContours(contourImg, [result], -1, (0,255,0), 3) 
-    else:
-        print("Contours not found")
-    #-------This part is only for debugging purpose--------
-    contourImage = None
+            quadri.append(approx)
+    #-------This part is for debugging purpose only--------
     if debug != None:
         debug.append(edges)
-        contourImage = img.copy()
-    contourPoints = np.array([x[0] for x in result], dtype = "float32");
-    topLeft, topRight, bottomRight, bottomLeft = getOrderPoints(contourPoints)
-    oldCorners = np.array([topLeft, topRight, bottomRight, bottomLeft], dtype = "float32")
-    cv.imwrite('./output/contours.png', contourImg)
-    #Compute new width and height
-    newWidth = max(getDistance(topLeft, topRight), getDistance(bottomLeft, bottomRight))
-    newHeight = max(getDistance(topLeft, bottomLeft), getDistance(topRight, bottomRight))
-    #Compute 4 new corners
-    newCorners = np.array([
-        [0, 0],
-        [newWidth - 1, 0],
-        [newWidth - 1, newHeight -1],
-        [0, newHeight -1]], dtype = "float32")
-    #Compute transformation matrix
-    transMat = cv.getPerspectiveTransform(oldCorners, newCorners)
-    #Transform
-    resultImage = cv.warpPerspective(img, transMat, (newWidth, newHeight))
-    cv.imwrite('./output/output.png', resultImage)
+        allContoursImg = img.copy()
+        cv.drawContours(allContoursImg, contours, -1, (0,255,0), 3)
+        debug.append(allContoursImg)
+    #------------------------------------------------------
+    if len(quadri) > 0:
+        detectedContour = max(quadri, key = getArea)
+        contourPoints = np.array([x[0] for x in detectedContour], dtype = "float32");
+        topLeft, topRight, bottomRight, bottomLeft = getOrderPoints(contourPoints)
+        oldCorners = np.array([topLeft, topRight, bottomRight, bottomLeft], dtype = "float32")
+        #Compute new width and height
+        newWidth = max(getDistance(topLeft, topRight), getDistance(bottomLeft, bottomRight))
+        newHeight = max(getDistance(topLeft, bottomLeft), getDistance(topRight, bottomRight))
+        #Compute 4 new corners
+        newCorners = np.array([
+            [0, 0],
+            [newWidth - 1, 0],
+            [newWidth - 1, newHeight -1],
+            [0, newHeight -1]], dtype = "float32")
+        #Compute transformation matrix
+        transMat = cv.getPerspectiveTransform(oldCorners, newCorners)
+        #Transform
+        resultImage = cv.warpPerspective(img, transMat, (newWidth, newHeight))
+        #Debugging purpose------------------------------------------------------
+        if debug != None:
+            detectContourImg = img.copy()
+            cv.drawContours(detectContourImg, [detectedContour], -1, (255,0,0), 3)
+            debug.append(detectContourImg)
+        #-----------------------------------------------------------------------
+        return resultImage
+    else:
+        if debug != None:
+            debug.append(img)
+        raise Exception('Contours not found')
