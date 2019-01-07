3fd6bacd0816951f555dca2e23df3313eec9b739
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-01-08 06:20:31 +0700

    Update

--- src/preprocess.py
+++ src/preprocess.py
@@ -94,9 +94,12 @@ def getCorners(sides, houghSpace):
         i += 1
     return corners
 def changeView(originalImg, Q = 3):
+    def getLength(contour):
+        return cv.arcLength(contour, False)
     height = originalImg.shape[0]
     width = originalImg.shape[1]
     img = originalImg
+    ratio = 1
     if width > 1000:
         ratio = 500 / width
         img = cv.resize(originalImg, None, fx = ratio, fy = ratio, interpolation = cv.INTER_LINEAR) 
@@ -107,12 +110,104 @@ def changeView(originalImg, Q = 3):
     threshold1 = 50
     threshold2 = 200
     edges = cv.Canny(blur, threshold1, threshold2)
-    contourImg, contours, hierarchy = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    maxcontour = max(contours, cv.contourArea)
-    cv.drawContours(edges, [contour], -1, (0,255,0), 3)
-    cv.imwrite('./output/largest.png', edges)
+    contourImg, contours, hierarchy = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    #maxcontour = max(contours, key = getLength)
+    #maxcontour = contours[0]
+    #print(getLength(maxcontour))
+    maxcontour = max(contours, key = getLength)
+    #print(getLength(maxcontour))
+    #edges = cv.cvtColor(edges, cv.COLOR_GRAY2RGB)
+    black = np.zeros((int(height * ratio), int(width * ratio)), "uint8")
+    cv.drawContours(black, [maxcontour], -1, (255,255,255), 1)
+    dilate = cv.dilate(black, (7,7))
+    erode = cv.erode(dilate, (7,7))
+    contourImg, contours, hierarchy = cv.findContours(erode, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    maxcontour = max(contours, key = getLength)
+    cv.imwrite('./output/erode.png', erode)
+    error = 0.1
+    epsilon = error * cv.arcLength(maxcontour, True)
+    approx = cv.approxPolyDP(maxcontour, epsilon, True)
+    cv.cvtColor(black, cv.COLOR_GRAY2RGB)
+    #cv.drawContours(img, [approx], -1, (0,255,0), 3)
+    #cv.drawContours(img, contours, -1, (0,0,255), 3)
+    cv.imwrite('./output/largest.png', black)
+    minvote = 0
+    maxvote = 500
     '''
-    houghSpace, maxval = houghTransform(edges)
+    while True:
+        vote = int((minvote + maxvote) / 2)
+        lines = cv.HoughLines(erode,1,np.pi/180,vote)
+        numlines = len(lines)
+        if numlines > 4:
+            minvote = vote
+        elif numlines < 4:
+            maxvote = vote
+        else:
+            break
+            '''
+    houghImg = np.zeros((int(height * ratio), int(width * ratio)), "uint8")
+    lines = cv.HoughLines(erode,1,np.pi/180,50)
+    #print(lines)
+    correctLines = list()
+    thetaErr = math.pi / 6
+    rhoErr = 180
+    for line in lines:
+        rho,theta = line[0]
+        isNew = True
+        for l in correctLines:
+            if abs(theta - l[1]) < thetaErr and abs(rho - l[0]) < rhoErr:
+                isNew = False
+        if isNew:
+            correctLines.append((rho, theta))
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 2000*(-b))
+            y1 = int(y0 + 2000*(a))
+            x2 = int(x0 - 2000*(-b))
+            y2 = int(y0 - 2000*(a))
+            cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+            cv.line(houghImg, (x1,y1), (x2, y2), (255,255,255), 1)
+        else:
+            continue
+    print(correctLines)
+    contourImg, contours, hierarchy = cv.findContours(houghImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    error = 0.01
+    quadri = list()
+    for contour in contours:
+        epsilon = error * cv.arcLength(contour, True)
+        approx = cv.approxPolyDP(contour, epsilon, True)
+        if len(approx) == 4:
+            quadri.append(approx)
+    houghImg = cv.cvtColor(houghImg, cv.COLOR_GRAY2RGB)
+    if len(quadri) == 0:
+        print("Error, contours not found")
+        cv.drawContours(houghImg, contours, -1, (255,255,0), 2)
+        return originalImg
+    else:
+        maxcontour = max(quadri, key = getArea)
+        cv.drawContours(houghImg, [maxcontour], -1, (0,255,0), 2)
+        cv.imwrite('./output/hough.png', houghImg)
+        contourPoints = np.array([x[0] for x in maxcontour], dtype = "float32")
+        topLeft, topRight, bottomRight, bottomLeft = getOrderPoints(contourPoints)
+        oldCorners = np.array([topLeft, topRight, bottomRight, bottomLeft], dtype = "float32")
+        #Compute new width and height
+        newWidth = max(getDistance(topLeft, topRight), getDistance(bottomLeft, bottomRight))
+        newHeight = max(getDistance(topLeft, bottomLeft), getDistance(topRight, bottomRight))
+        #Compute 4 new corners
+        newCorners = np.array([
+            [0, 0],
+            [newWidth - 1, 0],
+            [newWidth - 1, newHeight -1],
+            [0, newHeight -1]], dtype = "float32")
+        #Compute transformation matrix
+        transMat = cv.getPerspectiveTransform(oldCorners, newCorners)
+        #Transform
+        resultImage = cv.warpPerspective(img, transMat, (newWidth, newHeight))
+        return resultImage
+    '''
+    houghSpace, maxval = houghTransform(erode)
     sides = getSides(houghSpace, maxval / Q)
     corners = getCorners(sides, houghSpace)
     print(corners)
@@ -139,6 +234,4 @@ def changeView(originalImg, Q = 3):
     #Transform
     resultImage = cv.warpPerspective(img, transMat, (newWidth, newHeight))
     '''
-    resultImage = img
-    return resultImage
 
