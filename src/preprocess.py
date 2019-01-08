96b5c605ec4016c21c5dab97a331a41505a614ff
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-01-08 17:23:11 +0700

    Update case 3 edges

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,7 +3,7 @@ from scipy import stats
 import numpy as np
 import math
 from scipy import ndimage
-debug = False
+debug = True
 def getDistance(a,b):
     return np.linalg.norm(a - b)
 def getArea(contour):
@@ -27,9 +27,49 @@ def getOrderPoints(points):
         bottomRight = right[1]
         topRight = right[0]
     return (topLeft, topRight, bottomRight, bottomLeft)
+def getIntersection(line1, line2):
+    #lines are of the form (rho, theta)
+    print(line1)
+    print(line2)
+    rho1, theta1 = line1
+    rho2, theta2 = line2
+    A = np.array([[math.cos(theta1), math.sin(theta1)],
+                [math.cos(theta2), math.sin(theta2)]])
+    B = np.array([rho1, rho2]) 
+    print(A)
+    print(B)
+
+    #return form: np.array([x, y])
+    try:
+        result = np.linalg.solve(A, B) 
+    except LinAlgError:
+        return (math.inf, math.inf)
+    else:
+        return result
 def changeView(originalImg):
     def getLength(contour):
         return cv.arcLength(contour, False)
+    def checkSimilarAngle(theta1, theta2):
+        if theta1 <= thetaErr / 2 and theta2 >= math.pi - thetaErr / 2:
+            return True, (theta1, theta2)
+        elif theta2 <= thetaErr / 2 and theta1 >= math.pi - thetaErr / 2:
+            return True, (theta2, theta1)
+        else:
+            return False, None
+    #get missing edges in case only three edges are detected
+    #return [lonely edges, pair edges]
+    def getMissingEdges(correctLines):
+        if checkSimilarAngle(correctLines[0][1], correctLines[1][1]):
+            return [correctLines[2], correctLines[0], correctLines[1]]
+        elif checkSimilarAngle(correctLines[0][1], correctLines[2][1]):
+            return [correctLines[1], correctLines[0], correctLines[2]]
+        else:
+            return correctLines
+    def getRho(theta):
+        def result(point):
+            #point is of the form (x, y)
+            return point[0] * math.cos(theta) + point[1] * math.sin(theta)
+        return result
     height = originalImg.shape[0]
     width = originalImg.shape[1]
     img = originalImg
@@ -55,14 +95,12 @@ def changeView(originalImg):
     kernel = (7,7)
     closing = cv.morphologyEx(black, cv.MORPH_CLOSE, kernel)
     contourImg, contours, hierarchy = cv.findContours(closing, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    if debug == True:
-        cv.imwrite('./output/closing.png', closing)
     cv.cvtColor(black, cv.COLOR_GRAY2BGR)
     houghImg = np.zeros((height, width), "uint8")
     lines = cv.HoughLines(closing,1,np.pi/180,50)
     correctLines = list()
-    thetaErr = math.pi / 6
-    rhoErr = 50
+    thetaErr = math.pi / 4
+    rhoErr = 50 #need to change this mechanism to work with 021.png
     diagonal = math.sqrt(height ** 2 + width ** 2)
     for line in lines:
         if len(correctLines) == 4:
@@ -70,31 +108,74 @@ def changeView(originalImg):
         rho,theta = line[0]
         isNew = True
         for l in correctLines:
-            if theta <= thetaErr / 2 and l[1] >= math.pi - thetaErr / 2:
-                if abs(rho + l[0]) < rhoErr and abs(theta + math.pi - l[1]) < thetaErr:
+            check = checkSimilarAngle(theta, l[1])
+            if check[0]:
+                theta1, theta2 = check[1]
+                if abs(rho + l[0]) < rhoErr and abs(theta1 + math.pi - theta2) < thetaErr:
                     isNew = False
-            elif l[1] <= thetaErr / 2 and theta >= math.pi - thetaErr / 2:
-                if abs(rho + l[0]) < rhoErr and abs(l[1] + math.pi - theta) < thetaErr:
-                    isNew = False
-            else:
-                if abs(theta - l[1]) < thetaErr and abs(rho - l[0]) < rhoErr:
-                    isNew = False
+            elif abs(theta - l[1]) < thetaErr and abs(rho - l[0]) < rhoErr:
+                isNew = False
         if isNew:
-            correctLines.append((rho, theta))
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho / ratio
-            y0 = b*rho / ratio
-            x1 = int(x0 + math.ceil(diagonal) * (-b))
-            y1 = int(y0 + math.ceil(diagonal) * a)
-            x2 = int(x0 - math.ceil(diagonal) * (-b))
-            y2 = int(y0 - math.ceil(diagonal) * a)
-            cv.line(houghImg, (x1,y1), (x2, y2), (255,255,255), 1)
+            correctLines.append([rho, theta])
         else:
             continue
+    numLines = len(correctLines)
+    if numLines < 3:
+        print("Error: Receipt does not have enough edges to detect!")
+    elif numLines == 3:
+        correctLines = getMissingEdges(correctLines)
+        for line in correctLines:
+            line[0] = line[0] / ratio
+        mainRho = correctLines[0][0]
+        mainTheta = correctLines[0][1]
+        leftBound = (0, 0)
+        rightBound = (width, 0)
+        topBound = (0, math.pi / 2)
+        bottomBound = (height, math.pi / 2)
+        bounds = (leftBound, rightBound, topBound, bottomBound)
+        intersections = list()
+        print(correctLines)
+        print(bounds)
+        for bound in bounds:
+            for i in range(1,3):
+                intersections.append(getIntersection(correctLines[i], bound))
+        try:
+            intersections.remove(math.inf)
+        except ValueError:
+            pass
+        for point in intersections:
+            cv.circle(originalImg, (int(point[1]), int(point[0])), 3, (0,255,0), 3)
+        if debug:
+            cv.imwrite('./output/original.png', originalImg)
+        keysort = getRho(mainTheta)
+        intersections.sort(key = keysort)
+        print(intersections)
+        rho1 = keysort(intersections[int(len(intersections) / 2) - 1])
+        rho2 = keysort(intersections[int(len(intersections) / 2)])
+        print(rho1)
+        print(rho2)
+        if abs(mainRho - rho1) > abs(mainRho - rho2):
+            correctLines.append((rho1, mainTheta))
+        else:
+            correctLines.append((rho2, mainTheta))
+    for line in correctLines:
+        rho, theta = line
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + math.ceil(diagonal) * (-b))
+        y1 = int(y0 + math.ceil(diagonal) * a)
+        x2 = int(x0 - math.ceil(diagonal) * (-b))
+        y2 = int(y0 - math.ceil(diagonal) * a)
+        cv.line(houghImg, (x1,y1), (x2, y2), (255,255,255), 3)
     if debug == True:
         print(correctLines)
-    contourImg, contours, hierarchy = cv.findContours(houghImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    kernel = (7,7)
+    closing = cv.morphologyEx(houghImg, cv.MORPH_CLOSE, kernel)
+    if debug == True:
+        cv.imwrite('./output/closing.png', closing)
+    contourImg, contours, hierarchy = cv.findContours(closing, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     error = 0.01
     quadri = list()
     for contour in contours:
@@ -138,5 +219,5 @@ def changeView(originalImg):
         return resultImage
 def binarize(img):
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    thresh = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-    return thresh
+    #thresh = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    return img
