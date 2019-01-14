1aa60a815da967c908a1d9bd84ec2f431a08733c
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-01-14 09:14:05 +0700

    Revert "Update"
    This reverts commit eb45583fa398d7509b8df8ecdfad5ef404bb79bb.

--- src/detection.py
+++ src/detection.py
@@ -1,29 +1,18 @@
 import cv2 as cv
 import numpy as np
-import preprocess as pre
-import matplotlib.pyplot as plt
-import time
 
 def findText(img):
-    start = time.time()
-    black = np.zeros((img.shape[0], img.shape[1]), "uint8")
-    kernel = np.ones((1,15),np.uint8)
-    dst = cv.cornerHarris(img,2,3,0.04)
-    dst = cv.dilate(dst,kernel)
-    black[dst>0.01*dst.max()]= 255
-    closing = cv.morphologyEx(black, cv.MORPH_CLOSE, kernel)
-    cv.imshow('Closing', closing)
+    '''
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    '''
+    threshold1 = 50
+    threshold2 = 200
+    canny = cv.Canny(img, threshold1, threshold2)
+    kernel = np.ones((5,5),np.uint8)
+    dilation = cv.dilate(canny,kernel,iterations = 1)
+    closing = cv.morphologyEx(dilation, cv.MORPH_CLOSE, kernel)
+    img, contours, hierarchy = cv.findContours(closing, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cv.imshow('Test', closing)
     cv.waitKey(0)
     cv.destroyAllWindows()
-    closing, contours, hierarchy = cv.findContours(closing, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-    minwidth = 7
-    for cnt in contours:
-        x,y,w,h = cv.boundingRect(cnt)
-        if h > minwidth:
-            y -= 2
-            h += 4
-            cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-    end = time.time()
-    print(end - start)
-    cv.imwrite('./output/box.png', img)
+
