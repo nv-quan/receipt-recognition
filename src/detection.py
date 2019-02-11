af7f936c6951f7925bec660bc642471032a0a9d8
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-02-11 12:05:02 +0700

    Update

--- src/detection.py
+++ src/detection.py
@@ -3,9 +3,9 @@ import numpy as np
 import preprocess as pre
 import matplotlib.pyplot as plt
 import time
-
+import json
+report = False
 def findText(img):
-    #start = time.time()
     black = np.zeros((img.shape[0], img.shape[1]), "uint8")
     kernel = np.ones((1,15),np.uint8)
     dst = cv.cornerHarris(img,2,3,0.04)
@@ -15,12 +15,19 @@ def findText(img):
     closing, contours, hierarchy = cv.findContours(closing, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     minwidth = 7
+    textBoxes = list()
     for cnt in contours:
         x,y,w,h = cv.boundingRect(cnt)
         if h > minwidth:
+            info = (x,y,w,h)
+            textBoxes.append(info)
             y -= 2
             h += 4
             cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-    #end = time.time()
-    #print(end - start)
-    cv.imwrite('./output/box.png', img)
+    if report:
+        cv.imwrite('./output/box.png', img)
+        boxesFile = open('./output/textboxes.json','w')
+        boxesFile.write(json.dumps(textBoxes, indent = 4))
+        boxesFile.close()
+    return textBoxes
+def compare(rect1, rect2):
--- src/main.py
+++ src/main.py
@@ -2,15 +2,48 @@ import cv2 as cv
 import numpy as np
 import sys
 import preprocess as pre
-import detection as dtc
+import detection as dt
+import json
 
-inp = sys.argv[1]
-if len(sys.argv) > 2:
-    out = sys.argv[2]
-else:
-    out = 'output'
-img = cv.imread(inp)
-changeview = pre.changeView(img)
-output = pre.binarize(changeview)            
-dtc.findText(output)
-cv.imwrite('./output/' + out + '.png', output)
+#Main function
+def main():
+    sample = json.load(sampleFile)
+    img = cv.imread(inp)
+    pre.debug = debugFlag
+    pre.report = reportFlag
+    changeview = pre.changeView(img)
+    if reportFlag:
+        cv.imwrite('./output/changeview.png', changeview)
+    stdH = config['document']['height']
+    stdW = config['document']['width']
+    stdSizeImg = cv.resize(changeview, (stdW, stdH))
+    gray = cv.cvtColor(stdSizeImg, cv.COLOR_BGR2GRAY)
+    dt.report = reportFlag
+    textBoxes = dt.findText(gray)
+
+#Global constant (do not change)
+debugFlag = False
+reportFlag = False
+inp = None
+sampleFile = None
+
+#Load and run configuration
+configFile = open('./src/config.json','r')
+config = json.load(configFile)
+sampleFile = open(config['sample'], 'r')
+#run mode
+if 'debug' in config['run mode']:
+    debugFlag = True
+if 'report' in config['run mode']:
+    reportFlag = True
+#input mode
+if config['input mode'] == 'single':
+    inp = config['input file']
+    main()
+elif config['input mode'] == 'folder':
+    pass
+#output
+if config['output name'] = 'input':
+    pass
+configFile.close()
+sampleFile.close()
